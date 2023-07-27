4fdf4551e8c0e3c3bcdc5a0b6ae0b3dde2fbb08b
__hydra__ <62038340+jorgecosta97@users.noreply.github.com>
2023-07-27 12:23:13 +0100

    Update Hourly_frequency.py
    update

--- Hourly_frequency.py
+++ Hourly_frequency.py
@@ -8,8 +8,8 @@ input_data = [[13, 7],
 def routespertime(indata, t, dt):
     n_routes = 0
     for n in range(0, len(indata)):
-        a = indata[n][0]  # start time
-        b = indata[n][1]  # duration
+        a = indata[n][0]  # Endtime
+        b = indata[n][1]  # Duration
         if (a>=t or a-b<0) and (t > a-b-dt %24): n_routes+=1
     return n_routes
 
