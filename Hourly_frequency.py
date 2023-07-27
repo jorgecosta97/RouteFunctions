c3de527c955cd139cefdd3790078b10deeba8d62
__hydra__ <62038340+jorgecosta97@users.noreply.github.com>
2023-07-27 13:11:42 +0100

    Update Hourly_frequency.py
    update

--- Hourly_frequency.py
+++ Hourly_frequency.py
@@ -1,16 +1,24 @@
-### Array with the end time and duration of the route.
-input_data = [[13, 7],
-              [18, 10],
-              [8, 7],
-              [23, 10],
-              [2, 7]] 
+import pandas as pd
+import math
 
-def routespertime(indata, t, dt):
-    n_routes = 0
-    for n in range(0, len(indata)):
-        a = indata[n][0]  # Endtime
-        b = indata[n][1]  # Duration
-        if (a>=t or a-b<0) and (t > a-b-dt %24): n_routes+=1
-    return n_routes
+def routespertime(indata, dt): #indata - array with end time and duration in decimal format; dt - time interval in decimal format 1 => 1 hour
+    t = 0
+    tp = []
+    vn = []
+    pc = []
+    while t!=24:
+        r = 0
+        for n in range(0, len(indata)):
+            a = indata[n][0]  # end time
+            b = indata[n][1]  # duration
+            if (a>=t or a-b<0) and (t > a-b-dt %24): r+=1
+        f, i = math.modf(t)
+        f1, i1 = math.modf(t+dt)
+        tp.append(f"{int(i):02}:{int(f*60):02} - {int(i1):02}:{int(f1*60):02}")
+        vn.append(r)
+        pc.append((r/len(indata))*100)
+        t+=dt
+    dict = {'time_Period': tp, 'Routes (N.)': vn, 'Routes (%)': pc}
+    out = pd.DataFrame(dict)
+    return out
 
-print (routespertime(input_data, 17, 1))
